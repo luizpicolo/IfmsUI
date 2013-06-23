c17ca55a71fe35ff73e51f08bbc331c02034f2e3
picolo <picolo@Picolo>
2013-06-23 15:59:50 -0400

    Adicionado diretório INC para inclusão das páginas secundárias

---
+++ inc/sobre.php
@@ -0,0 +1,6 @@
+<section>
+    <h1>Principal</h1>
+    <p>
+        A IFMS User Interface, ou IFMS-UI, é uma interface gráfica em desenvolvimento que visará atender projetos de iniciação científica desenvolvidos por alunos/pesquisadores do Instituto Federal de Mato Grosso do Sul - Campus de Nova Andradina. Tem como foco central o desenvolvimento de uma interface minimalista e amigável, a qual priorizará a facilidade do uso e os princípios básicos de usabilidade.
+    </p>
+</section>
