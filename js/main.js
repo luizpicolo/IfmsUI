800ce3dd27b390c233d371c9d4349f139c76aff1
picolo <picolo@Picolo>
2013-06-23 16:00:38 -0400

    Comentado codigo para sua inclusão apenas em versão que não sejam desktop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
-    $("#nav-btn").click(function() {
-        $("#nav").toggle("blind", 400);
-    });
-})
+//$(document).ready(function() {
+//    $("#nav-btn").click(function() {
+//        $("#nav").css("left", "0").toggle("slow");
+//        //$("#header").css("left", "0").toggle("slow");
+//    });
+//})
 
