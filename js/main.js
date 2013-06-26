0c41e2a918bb970f4f750c8434cc1f32950b8537
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-06-26 14:51:00 -0400

    Removido conteudo. O mesmo ainda não será utilizado

--- js/main.js
+++
@@ -1,7 +0,0 @@
-//$(document).ready(function() {
-//    $("#nav-btn").click(function() {
-//        $("#nav").css("left", "0").toggle("slow");
-//        //$("#header").css("left", "0").toggle("slow");
-//    });
-//})
-
