f6cdbde1869c1b8e71e63ce83b9d1ad0071e369e
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-06-26 14:53:00 -0400

    Removido código...

---
+++ inc/grids.php
@@ -0,0 +1,11 @@
+<section>
+    <hgroup>
+        <h1>Grids Responsivos</h1>
+        <h2>orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet</h2>
+    </hgroup>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    
+</section>
