c1bbb94f04b724e364477fb62605ab0533b2556f
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-06-26 14:50:19 -0400

    Arquivo para demostração das tabelas. Foi criada tabela padrão e a com bordas

---
+++ inc/tabelas.php
@@ -0,0 +1,104 @@
+<section>
+    <hgroup>
+        <h1>Grids Responsivos</h1>
+        <h2>orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet</h2>
+    </hgroup>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <h2>Tabela Padrão</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+        <table class="tabela">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>RGA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+            </tbody>
+        </table>
+        <code>
+            Aqui vai o código fonte
+        </code>
+    </div>
+
+    <h2>Tabela Padrão com Bordas</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+        <table class="tabela bordar-tabela">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>RGA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+            </tbody>
+        </table>
+        <code>
+            Aqui vai o código fonte
+        </code>
+    </div>
+
+</section>
