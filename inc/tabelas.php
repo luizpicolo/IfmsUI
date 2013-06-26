4cdfc1f39af77501dddedeebad051a961f007065
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-06-26 16:29:37 -0400

    Acrescentado código para tabela Zebrada

--- inc/tabelas.php
+++ inc/tabelas.php
@@ -100,5 +100,52 @@
             Aqui vai o código fonte
         </code>
     </div>
+    
+    <h2>Tabela Zebrada</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+        <table class="tabela tabela-zebrada">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>RGA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Lorem</td>
+                    <td>Lorem@Ipsum</td>
+                    <td>00000000</td>
+                </tr>
+            </tbody>
+        </table>
+        <code>
+            Aqui vai o código fonte
+        </code>
+    </div>
 
 </section>
