63d1bf30745bb72cdd1b64eb3357777ef3ea417f
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-06-27 10:43:42 -0400

    Arquivo para demostração dos botões.

---
+++ inc/botoes.php
@@ -0,0 +1,52 @@
+<section>
+    <hgroup>
+        <h1>Botões</h1>
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet</h2>
+    </hgroup>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <h2>Botões Padrão</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+        <table class="tabela bordar-tabela">
+            <thead>
+                <tr>
+                    <th>Botão</th>
+                    <th>Classe</th>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><button type="button" class="botao">Botão Padrão</button></td>
+                    <td>class="botao"</td>
+                    <td>Lorem Ipsum Dollar Medinas</td>
+                </tr>
+                <tr>
+                    <td><button type="button" class="botao botao-sucesso">Botão Sucesso</button></td>
+                    <td>class="botao botao-sucesso"</td>
+                    <td>Lorem Ipsum Dollar Medinas</td>
+                </tr>
+                <tr>
+                    <td><button type="button" class="botao botao-erro">Botão Erro</button></td>
+                    <td>class="botao botao-erro"</td>
+                    <td>Lorem Ipsum Dollar Medinas</td>
+                </tr>
+                <tr>
+                    <td><button type="button" class="botao botao-informacao">Botão Informação</button></td>
+                    <td>class="botao botao-informacao"</td>
+                    <td>Lorem Ipsum Dollar Medinas</td>
+                </tr>
+            </tbody>
+        </table>
+        <code>
+            Aqui vai o código fonte
+        </code>
+    </div>
+
+</section>
