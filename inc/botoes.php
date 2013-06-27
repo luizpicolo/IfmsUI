adadc9c4573f162a66255791258407c797de0fdf
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-06-27 14:26:07 -0400

    Adicionado Botões com tamanhos diferentes e Botões com ícones

--- inc/botoes.php
+++ inc/botoes.php
@@ -44,9 +44,60 @@
                 </tr>
             </tbody>
         </table>
-        <code>
-            Aqui vai o código fonte
-        </code>
     </div>
+    <div class="exemplo">
+<pre class="prettyprint">
+&lt;button type="button" class="botao"&gt;Botão Padrão&lt;/button&gt;
+&lt;button type="button" class="botao botao-sucesso"&gt;Botão Sucesso&lt;/button&gt;
+&lt;button type="button" class="botao botao-erro"&gt;Botão Erro&lt;/button&gt;
+&lt;button type="button" class="botao botao-informacao"&gt;Botão Informação&lt;/button&gt;
+</pre>
+    </div>
+    <h2>Botões com tamanhos diferentes</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
 
+    <div class="exemplo">
+        <button type="button" class="botao botao-extra-pequeno">Botão Extra Pequenao</button>
+        <button type="button" class="botao botao-pequeno">Botão Pequeno</button>
+        <button type="button" class="botao">Botão Padrão</button>
+        <button type="button" class="botao botao-largo">Botão Largo</button>
+        <button type="button" class="botao botao-extra-largo">Botão Extra Largo</button>    
+    </div>
+    <div class="exemplo">
+<pre class="prettyprint">
+&lt;button type="button" class="botao botao-extra-pequeno"&gt;Botão Extra Pequenao&lt;/button&gt;
+&lt;button type="button" class="botao botao-pequeno"&gt;Botão Pequeno&lt;/button&gt;
+&lt;button type="button" class="botao"&gt;Botão Padrão&lt;/button&gt;
+&lt;button type="button" class="botao botao-largo"&gt;Botão Largo&lt;/button&gt;
+&lt;button type="button" class="botao botao-extra-largo"&gt;Botão Extra Largo&lt;/button&gt;    
+</pre>
+    </div>
+    <h2>Botões com ícones</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+    <div class="exemplo">
+        <button type="button" class="botao botao-extra-pequeno">
+            <i class="icon-smile"></i>
+            Carinha Feliz
+        </button>
+        <button type="button" class="botao botao-pequeno">
+            <i class="icon-remove"></i>
+            Excluir
+        </button>
+        <button type="button" class="botao">
+            <i class="icon-music"></i>
+            Música
+        </button>
+        <button type="button" class="botao botao-largo">
+            <i class="icon-edit"></i>
+            Editar
+        </button>
+        <button type="button" class="botao botao-extra-largo">
+            <i class="icon-cog"></i>
+            Configurações
+        </button>    
+    </div>
 </section>
