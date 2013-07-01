7713218b7db44ee1a85b3690d82b8a161bc191f7
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-07-01 15:54:08 -0400

    Inserida paginação e alinhamentos

---
+++ inc/paginacao.php
@@ -0,0 +1,115 @@
+<section>
+    <hgroup>
+        <h1>Paginação</h1>
+        <h2>orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet</h2>
+    </hgroup>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <h2>Paginação Padrão</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+       <div class="paginacao">
+          <ul>
+            <li><a href="#">Anterior</a></li>
+            <li><a href="#" class="ativa">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">6</a></li>
+            <li><a href="#">Próximo</a></li>
+          </ul>
+        </div>
+    </div>
+    <div class="exemplo">
+<pre class="prettyprint">
+&lt;div class="paginacao"&gt;
+    &lt;ul&gt;
+        &lt;li&gt;&lt;a href="#"&gt;Anterior&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#" class="ativa"&gt;1&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#"&gt;2&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#"&gt;3&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#"&gt;4&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#"&gt;5&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#"&gt;6&lt;/a&gt;&lt;/li&gt;
+        &lt;li&gt;&lt;a href="#"&gt;Próximo&lt;/a&gt;&lt;/li&gt;
+    &lt;/ul&gt;
+&lt;/div&gt;
+</pre>
+    </div>
+    
+    <h2>Alinhamento Paginação</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+       <div class="paginacao paginacao">
+          <ul>
+            <li><a href="#">Anterior</a></li>
+            <li><a href="#" class="ativa">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">6</a></li>
+            <li><a href="#">Próximo</a></li>
+          </ul>
+        </div>
+        <div class="paginacao paginacao-centro">
+          <ul>
+            <li><a href="#">Anterior</a></li>
+            <li><a href="#" class="ativa">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">6</a></li>
+            <li><a href="#">Próximo</a></li>
+          </ul>
+        </div>
+        <div class="paginacao paginacao-direita">
+          <ul>
+            <li><a href="#">Anterior</a></li>
+            <li><a href="#" class="ativa">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">6</a></li>
+            <li><a href="#">Próximo</a></li>
+          </ul>
+        </div>
+    </div>
+    <div class="exemplo">
+<pre class="prettyprint">
+&lt;!-- Paginação Alinhada a esquerda --&gt;
+&lt;div class="paginacao paginacao"&gt;
+    &lt;ul&gt;
+        ...
+    &lt;/ul&gt;
+&lt;/div&gt;
+</pre> 
+<pre class="prettyprint">
+&lt;!-- Paginação Alinhada ao Centro --&gt;
+&lt;div class="paginacao paginacao-centro"&gt;
+    &lt;ul&gt;
+        ...
+    &lt;/ul&gt;
+&lt;/div&gt;
+</pre>
+<pre class="prettyprint">
+&lt;!-- Paginação Alinhada a Direita --&gt;
+&lt;div class="paginacao paginacao-direita"&gt;
+    &lt;ul&gt;
+        ...
+    &lt;/ul&gt;
+&lt;/div&gt;
+</pre>
+    </div>
+</section>
