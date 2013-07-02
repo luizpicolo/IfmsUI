cd1a77c817f69abbf8293d0ca4a56486ce875a70
Luiz Picolo <luizpicolo@luizpicolo.com.br>
2013-07-02 09:27:46 -0400

    Acrescentado código para alertas

---
+++ inc/alertas.php
@@ -0,0 +1,46 @@
+<section>
+    <hgroup>
+        <h1>Alertas</h1>
+        <h2>orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet</h2>
+    </hgroup>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <h2>Alerta Padrão</h2>
+    <div class="exemplo">
+        <div class="alerta alerta-aviso">
+            <div class="fechar"><i class="icon-remove"></i></div>
+            <strong>Aviso!</strong> Mensagem para verificação.
+        </div>
+        <div class="alerta alerta-erro">
+            <div class="fechar"><i class="icon-remove"></i></div>
+            <strong>Erro!</strong> Mensagem para verificação.
+        </div>
+        <div class="alerta alerta-sucesso">
+            <div class="fechar"><i class="icon-remove"></i></div>
+            <strong>Sucesso!</strong> Mensagem para verificação.
+        </div>
+    </div>
+    <div class="exemplo">
+<pre class="prettyprint">
+&lt;!-- Alerta de Aviso --&gt;
+&lt;div class="alerta alerta-aviso"&gt;
+    &lt;div class="fechar"&gt;&lt;i class="icon-remove"&gt;&lt;/i&gt;&lt;/div&gt;
+    &lt;strong&gt;Aviso!&lt;/strong&gt; Mensagem para verificação.
+&lt;/div&gt;
+
+&lt;!-- Alerta de Erro --&gt;
+&lt;div class="alerta alerta-erro"&gt;
+    &lt;div class="fechar"&gt;&lt;i class="icon-remove"&gt;&lt;/i&gt;&lt;/div&gt;
+    &lt;strong&gt;Erro!&lt;/strong&gt; Mensagem para verificação.
+&lt;/div&gt;
+
+&lt;!-- Alerta de Sucesso --&gt;
+&lt;div class="alerta alerta-sucesso"&gt;
+    &lt;div class="fechar"&gt;&lt;i class="icon-remove"&gt;&lt;/i&gt;&lt;/div&gt;
+    &lt;strong&gt;Sucesso!&lt;/strong&gt; Mensagem para verificação.
+&lt;/div&gt;
+</pre> 
+    </div>
+</section>
