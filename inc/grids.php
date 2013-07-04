e186028e4f0b751b59cbd415b27bc669ce678884
picolo <picolo@Picolo>
2013-07-04 00:29:17 -0400

    Inserido código para demostraçào da grade

--- inc/grids.php
+++ inc/grids.php
@@ -7,5 +7,50 @@
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
     </p>
 
-    
+    <h2>Grade Padrão</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dapibus nisi vitae sapien fringilla laoreet. Suspendisse potenti. Vestibulum vel accumsan mi. Suspendisse potenti. Suspendisse facilisis sed augue non pharetra. 
+    </p>
+
+    <div class="exemplo">
+        <div class="clearfix">
+            <div class="grade-12">12</div>	
+            <div class="grade-11">11</div>
+            <div class="grade-1">1</div>	
+            <div class="grade-10">10</div>
+            <div class="grade-2">2</div>	
+            <div class="grade-9">9</div>
+            <div class="grade-3">3</div>	
+            <div class="grade-8">8</div>
+            <div class="grade-4">4</div>	
+            <div class="grade-7">7</div>
+            <div class="grade-5">5</div>	
+            <div class="grade-6">6</div>
+            <div class="grade-6">6</div>
+            <div class="grade-5">5</div>
+            <div class="grade-7">7</div>	
+            <div class="grade-4">4</div>
+            <div class="grade-8">8</div>
+            <div class="grade-3">3</div>
+            <div class="grade-9">9</div>
+            <div class="grade-2">2</div>
+            <div class="grade-10">10</div>
+            <div class="grade-1">1</div>
+            <div class="grade-11">11</div>
+            <div class="grade-12">12</div>
+        </div>
+    </div>
+    <div class="exemplo">
+        <pre class="prettyprint">
+&lt;div class="grade-12"&gt;12&lt;/div&gt;	
+&lt;div class="grade-11"&gt;1&lt;/div&gt;
+&lt;div class="grade-1"&gt;1&lt;/div&gt;	
+&lt;div class="grade-10"&gt;10&lt;/div&gt;
+...
+&lt;div class="grade-1"&gt;1&lt;/div&gt;
+&lt;div class="grade-11"&gt;11&lt;/div&gt;
+&lt;div class="grade-12"&gt;12&lt;/div&gt;
+        </pre>
+    </div>
+
 </section>
